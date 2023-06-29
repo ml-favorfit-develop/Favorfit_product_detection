a61087124f270f34a321f8cb94abac46fe2ffdeb
ml-favorfit-develop <122414946+MiddleKD@users.noreply.github.com>
2023-06-29 11:29:44 +0900

    Error 수정 및 기능 추가
    로그 상세화, skipindex 기능 추가, label옵션 추가

--- run_model/dataset.py
+++ run_model/dataset.py
@@ -4,19 +4,24 @@ from pymongo import MongoClient, ASCENDING
 from PIL import Image
 
 class RemoveBackgroundDataset(Dataset):
-    def __init__(self, DB_HOST, DB_NAME, COLLECTION_NAME, target_label=None):
+    def __init__(self, DB_HOST, DB_NAME, COLLECTION_NAME, skip_index=None, target_label=None):
         client = MongoClient(DB_HOST)
         db = client[DB_NAME]
         collection = db[COLLECTION_NAME]
 
         if target_label == None:
-            docs = collection.find({}).sort("_id", ASCENDING)
+            cursor = collection.find({}).sort("_id", ASCENDING)
         else:
-            docs = collection.find({"label":{"$eq":target_label}}).sort("_id", ASCENDING)
+            cursor = collection.find({"label":{"$eq":target_label}}).sort("_id", ASCENDING)
 
-        self.images = [(self.make_path(doc), doc["tag"][0], doc["file_name"], doc["ext"]) for doc in docs]
+        if skip_index != None: 
+            for _ in range(skip_index+1): cursor.next()
+
+        self.images = [(self.make_path(doc), doc["tag"][0], doc["file_name"], doc["ext"]) for doc in cursor]
 
         client.close()
+
+        print(f"Dataset -> label:{target_label}, skip_index:{skip_index}, num_of_data:{len(self.images)}")
 
     def make_path(self, document):
         path = os.path.join("/",
@@ -26,7 +31,7 @@ class RemoveBackgroundDataset(Dataset):
                         document["tag"][0],
                         document["file_name"])
         return path
-
+        
     def __getitem__(self, idx):
         image_path, tag, file_name, ext = self.images[idx]
         try:
@@ -34,6 +39,20 @@ class RemoveBackgroundDataset(Dataset):
             return image, image_path, tag, file_name, ext
         except:
             return None, "Error", "Error", "Error", "Error"
+
+    # def __getitem__(self, idx):
+    #     if isinstance(idx, slice):
+    #         return [self.__get_single_item(i) for i in range(*idx.indices(len(self)))]
+    #     else:
+    #         return self.__get_single_item(idx)
+
+    # def __get_single_item(self, idx):
+    #     image_path, tag, file_name, ext = self.images[idx]
+    #     try:
+    #         image = Image.open(image_path).convert('RGB')
+    #         return image, image_path, tag, file_name, ext
+    #     except:
+    #         return None, "Error", "Error", "Error", "Error"
         
     def __len__(self):
         return len(self.images)
--- run_model/run_removebg.py
+++ run_model/run_removebg.py
@@ -3,13 +3,15 @@ import dataset, logger
 import os
 import torch
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from dotenv import load_dotenv
 from tqdm import tqdm
 import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--skipindex', type=int, default=-1, help='skip image index')
+parser.add_argument('--skipindex', type=int, default=None, help='skip image index')
+parser.add_argument('--label', type=str, default=None, help='target label')
 args = parser.parse_args()
 
 device = "cpu"
@@ -23,16 +25,17 @@ def set_device():
     return device
 
 
-def process(model, dataloader, skip_batch=-1):
+def process(model, dataloader):
     
     model.eval()
-    pbar = tqdm(total=len(dataloader), leave=False)
 
     with torch.no_grad():
+
+        pbar = tqdm(total=len(dataloader), leave=False)
         for batch, (image, img_path, tag, file_name, ext) in enumerate(dataloader):
+
             pbar.set_description(f"{batch} {file_name}: ")
-            if batch <= skip_batch:
-                pass
+
             if torch.cuda.is_available():
                 try:
                     image = image.to(device)
@@ -40,15 +43,15 @@ def process(model, dataloader, skip_batch=-1):
                     pass
             
             if img_path == None:
-                logger.save_log(f"{batch} , {img_path} , NONE")
+                logger.save_log(f"{batch+args.skipindex+1},{img_path},{args.label},NONE")
             else:
                 try:
                     output = model(image)
                     
                     logger.copy_and_save_image(image_array=output, image_path=img_path, tag=tag, file_name=file_name + "_mask", ext=ext)
-                    logger.save_log(f"{batch},{img_path},SUCCESS")
+                    logger.save_log(f"{batch+args.skipindex+1},{img_path},{args.label},SUCCESS")
                 except:
-                    logger.save_log(f"{batch},{img_path},FAILED")
+                    logger.save_log(f"{batch+args.skipindex+1},{img_path},{args.label},FAILED")
             
             pbar.update()
             
@@ -60,15 +63,16 @@ def run():
                         DB_HOST=os.getenv("DB_HOST"),
                         DB_NAME=os.getenv("DB_NAME"),
                         COLLECTION_NAME=os.getenv("COLLECTION_NAME"),
-                        target_label=None)
+                        skip_index=args.skipindex,
+                        target_label=args.label)
     
     # process_loader = DataLoader(process_dataset, batch_size=1, num_workers=4, pin_memory=True, shuffle=False)
     process_loader = process_dataset    # inspyrenet의 경우
 
     model = remove_background.BackGroundRemover(base_model="inspyrenet", 
-                                                fast=True, jit=True, device=device)
+                                                fast=True, jit=False, device=device)
 
-    process(model, process_loader, skip_batch=args.skipindex)
+    process(model, process_loader)
 
 
 if __name__=="__main__":
